0f5908cf438451f26fab0230ec355c9ad6098a89
Onur Sahin <anionrush@gmail.com>
2016-06-21 19:49:26 -0500

    Checkpoint 10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         
         /* @desc SongPlayer object to pass public methods
         *  @type {Object} */
@@ -37,6 +37,12 @@
                 formats: ['mp3'],
                 preload: true
             });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
  
             SongPlayer.currentSong = song;
         };
@@ -54,6 +60,9 @@
             SongPlayer.currentSong.playing = null;
         };
         
+        /* @desc Current playback time (in seconds) of currently playing song
+        *  @type {Number}*/
+        SongPlayer.currentTime = null;
         
         /* @desc Sets song that is currently playing
         *  @type {Object} */
@@ -113,11 +122,20 @@
             }
         };
         
+        /* @function setCurrentTime
+        *  @desc Set current time (in seconds) of currently playing song
+        *  @param {Number} time*/
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
             return SongPlayer;
     }
 
  
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
